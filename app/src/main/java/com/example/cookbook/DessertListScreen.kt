49e7af51ef3681bf11db70c00b5c7be4e1672127
BeatrisO <beatris_oliveira97@hotmail.com>
2025-11-24 15:46:02 -0300

    Adiciona animação de clique ao card

--- app/src/main/java/com/example/cookbook/DessertListScreen.kt
+++ app/src/main/java/com/example/cookbook/DessertListScreen.kt
@@ -1,20 +1,26 @@
 package com.example.cookbook.ui.screens
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,11 +66,29 @@ fun DessertListScreen(
 
 @Composable
 fun DessertCard(meal: Meal, onClick: () -> Unit) {
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 0.97f else 1f,
+        label = ""
+    )
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .height(120.dp)
-            .clickable { onClick() },
+            .graphicsLayer(
+                scaleX = scale,
+                scaleY = scale
+            )
+            .clickable(
+                interactionSource = interactionSource,
+                indication = ripple(bounded = true)
+            ) {
+                onClick()
+            },
         shape = RoundedCornerShape(12.dp),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface
@@ -91,6 +115,7 @@ fun DessertCard(meal: Meal, onClick: () -> Unit) {
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun DessertListScreenPreview() {
