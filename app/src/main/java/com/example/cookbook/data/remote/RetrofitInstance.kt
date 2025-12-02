9a81b6d9dc541abc68437359f152a8d3fcfdf9e2
BeatrisO <beatris_oliveira97@hotmail.com>
2025-12-02 17:05:30 -0300

    Cria RetrofitInstance reutilizável seguindo boas práticas de arquitetura

--- app/src/main/java/com/example/cookbook/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/cookbook/data/remote/RetrofitInstance.kt
@@ -5,11 +5,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    val api: MealApi by lazy {
+
+    private const val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
+
+    private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("https://www.themealdb.com/api/json/v1/1/")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(MealApi::class.java)
+    }
+
+    val api: MealApi by lazy {
+        retrofit.create(MealApi::class.java)
     }
 }
